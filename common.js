8913a11579334d8672bd7c15a826e5542dcb64b5
mamonovvlad <vladik301096>
2019-04-17 21:18:38 +0300

    проба

---
+++ common.js
@@ -0,0 +1,46 @@
+// ///////////Отправка на почту смс
+// $(document).ready(function() {
+// 	$("#form").submit(function() {
+// 		$.ajax({
+// 			type: "POST",
+// 			url: "mail.php",
+// 			// success: function() {
+// 				// $('.btn').on('click', function(e){
+// 				// 	e.preventDefault();
+// 				// 	$('.wrapper').addClass('top');
+// 				// 	$('.wrapper').css({'transition': '.5s'});
+// 				// 	$('.shadow').fadeIn();
+// 				//  });
+				 
+// 				//  $('.close').on('click', function(e){
+// 				// 	e.preventDefault();
+// 				// 	$('.wrapper').removeClass('top');
+// 				// 	$('.wrapper').css({'transition': '1s'});
+// 				// 	$('.shadow').fadeOut();
+// 				//  });
+// 				//  $('.shadow').on('click', function(e){
+// 				// 	e.preventDefault();
+// 				// 	$('.wrapper').removeClass('top');
+// 				// 	$('.shadow').fadeOut();
+// 				//  });	 
+// 			// }
+// 		});
+// 		// $("#form").trigger("reset");
+// 		});
+// 	});
+// // });
+$(document).ready(function(){
+	$('#form').click(function(e){
+		 e.preventDefault();
+		 $.ajax({
+				url: "mail.php",
+			  	type: "post",
+					success: function() {
+			
+			
+			 	}
+		 });
+		 $("#form").trigger("reset");
+	});    
+
+});
