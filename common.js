a7ee733e828b2c5c91951e87b33279f0e147691f
mamonovvlad <vladik301096>
2019-04-23 16:46:58 +0300

    Update common.js

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$("#form").submit(function () {
 		$.ajax({
-			type: "GET",
+			type: "POST",
 			url: "mail.php",
 			data: $(this).serialize()
 		}).done(function () {
