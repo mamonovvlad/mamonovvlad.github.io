872f01eb39693d9b68ee985880d4b646460cfdc9
mamonovvlad <vladik301096>
2019-04-23 13:46:48 +0300

    mamonovvlad

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$("#form").submit(function () {
 		$.ajax({
-			type: "POST",
+			type: "GET",
 			url: "mail.php",
 			data: $(this).serialize()
 		}).done(function () {
