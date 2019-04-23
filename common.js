56201539ce9f651aadc04b56996b880067b68f5b
mamonovvlad <vladik301096>
2019-04-23 13:15:21 +0300

    mamonovvlad

--- common.js
+++ common.js
@@ -1,46 +1,31 @@
-// ///////////Отправка на почту смс
-// $(document).ready(function() {
-// 	$("#form").submit(function() {
-// 		$.ajax({
-// 			type: "POST",
-// 			url: "mail.php",
-// 			// success: function() {
-// 				// $('.btn').on('click', function(e){
-// 				// 	e.preventDefault();
-// 				// 	$('.wrapper').addClass('top');
-// 				// 	$('.wrapper').css({'transition': '.5s'});
-// 				// 	$('.shadow').fadeIn();
-// 				//  });
-				 
-// 				//  $('.close').on('click', function(e){
-// 				// 	e.preventDefault();
-// 				// 	$('.wrapper').removeClass('top');
-// 				// 	$('.wrapper').css({'transition': '1s'});
-// 				// 	$('.shadow').fadeOut();
-// 				//  });
-// 				//  $('.shadow').on('click', function(e){
-// 				// 	e.preventDefault();
-// 				// 	$('.wrapper').removeClass('top');
-// 				// 	$('.shadow').fadeOut();
-// 				//  });	 
-// 			// }
-// 		});
-// 		// $("#form").trigger("reset");
-// 		});
-// 	});
-// // });
-$(document).ready(function(){
-	$('#form').click(function(e){
-		 e.preventDefault();
-		 $.ajax({
-				url: "mail.php",
-			  	type: "post",
-					success: function() {
-                  alert('Ghbdtn');
-			
-			 	}
-		 });
-		 
-	});    
-   $("#form").trigger("reset");
+$(document).ready(function () {
+	$("#form").submit(function () {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function () {
+			$('.wrapper').addClass('top');
+			$('.wrapper').css({
+				'transition': '.5s'
+			});
+			$('.shadow').fadeIn();
+			$('.close').on('click', function (e) {
+				e.preventDefault();
+				$('.wrapper').removeClass('top');
+				$('.wrapper').css({
+					'transition': '1s'
+				});
+				$('.shadow').fadeOut();
+			});
+			$('.shadow').on('click', function (e) {
+				e.preventDefault();
+				$('.wrapper').removeClass('top');
+				$('.shadow').fadeOut();
+			});
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+
 });
